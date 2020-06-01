12793f6cc3f8ae0e994861098b6c6f27c9ee46f1
Rouzbeh Hatamy <roozbeh.hatamy@gmail.com>
2020-06-01 10:37:08 +0430

    add dynamic table

--- scripts.js
+++ scripts.js
@@ -1,13 +1,38 @@
-/* eslint-disable*/
+/* eslint-disable */
+// globals
+const contacts = [
+  {
+    id: '1',
+    name: 'rouzbeh',
+    surname: 'hatamy',
+    phoneNumber: '09339994444',
+    email: 'roozbeh.hatamy@gmail.com',
+    address: 'No.1 Hashemi st. Tehran IR',
+    birthday: '1996/06/11',
+    details: 'still a good friend',
+  },
+  {
+    id: '2',
+    name: 'sara',
+    surname: 'kamali',
+    phoneNumber: '09120000000',
+    email: 'sara.Oops@gmail.com',
+    address: 'unknown',
+    birthday: '1998/09/11',
+    details: 'hotie',
+  },
+];
 // show form
 function showForm() {
   $('#form').css('width', '20%');
   $('#form').css('visibility', 'visible');
+  $('#form').css('padding', '0 20px');
 }
 // hide form
 function hideForm() {
   $('#form').css('width', '0px');
   $('#form').css('visibility', 'hidden');
+  $('#form').css('padding', '0');
 }
 // toggle form
 function toggleForm() {
@@ -19,14 +44,14 @@ function toggleForm() {
   }
 }
 // on load data
-$(document).onLoad(
-  document
-    .getElementById('form_contact')
-    .addEventListener('submit', function(event) {
-      event.preventDefault();
-      hideForm();
-    })
-);
+$(document).ready(function() {
+  // on submit
+  $('#form_contact').on('submit', function(event) {
+    event.preventDefault();
+    hideForm();
+  });
+  renderTable();
+});
 // delete item
 function deletItem() {
   Swal.fire({
@@ -41,5 +66,24 @@ function deletItem() {
     if (result.value) {
       Swal.fire('Deleted!', 'Your file has been deleted.', 'success');
     }
+  });
+}
+// render table
+function renderTable() {
+  contacts.map((contact) => {
+    const tr = document.createElement('tr');
+    tr.innerHTML = `<td>${contact.id}</td>
+    <td>${contact.name}</td>
+    <td>${contact.surname}</td>
+  <td>${contact.phoneNumber}</td>
+  <td>${contact.email}</td>
+  <td>${contact.address}</td>
+  <td>${contact.birthday}</td>
+  <td>${contact.details}</td>
+  <td> <button type="button" onclick="deletItem()" class="btn btn-danger"><i
+  class="fa fa-trash" aria-hidden="true"></i></button>
+<button type="button" onclick="toggleForm()" class="btn btn-primary"><i
+  class="fa fa-pencil-square" aria-hidden="true"></i></button> </td>`;
+    return $('tbody').append(tr);
   });
 }
