eedc1e0d2adf46a1872fdade57392aa79d37954c
Rouzbeh Hatamy <roozbeh.hatamy@gmail.com>
2020-06-01 13:34:14 +0430

    add delete contact and local storage settings

--- scripts.js
+++ scripts.js
@@ -1,27 +1,31 @@
-/* eslint-disable */
-// globals
-const contacts = [
-  {
-    id: '1',
-    name: 'rouzbeh',
-    surname: 'hatamy',
-    phoneNumber: '09339994444',
-    email: 'roozbeh.hatamy@gmail.com',
-    address: 'No.1 Hashemi st. Tehran IR',
-    birthday: '1996/06/11',
-    details: 'still a good friend',
-  },
-  {
-    id: '2',
-    name: 'sara',
-    surname: 'kamali',
-    phoneNumber: '09120000000',
-    email: 'sara.Oops@gmail.com',
-    address: 'unknown',
-    birthday: '1998/09/11',
-    details: 'hotie',
-  },
-];
+// render table
+function renderTable() {
+  const contacts = JSON.parse(localStorage.getItem('contacts'));
+  if (!contacts) {
+    localStorage.setItem('contacts', JSON.stringify([]));
+    return;
+  }
+  contacts.map(contact => {
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+    <td>${contact.id}</td>
+    <td>${contact.name}</td>
+    <td>${contact.surname}</td>
+    <td>${contact.phoneNumber}</td>
+    <td>${contact.email}</td>
+    <td>${contact.address}</td>
+    <td>${contact.birthday}</td>
+    <td>${contact.details}</td>
+    <td> 
+    <button type="button" 
+    onclick="deletItem(${contact.id})" class="btn btn-danger"><i
+  class="fa fa-trash" aria-hidden="true"></i></button>
+<button type="button" onclick="toggleForm()" class="btn btn-primary"><i
+  class="fa fa-pencil-square" aria-hidden="true"></i></button> </td>`;
+    return $('tbody').append(tr);
+  });
+}
+
 // show form
 function showForm() {
   $('#form').css('width', '20%');
@@ -48,12 +52,33 @@ $(document).ready(function() {
   // on submit
   $('#form_contact').on('submit', function(event) {
     event.preventDefault();
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+    const newContactData = new FormData(event.target).entries();
+    const newContact = Object.fromEntries(newContactData);
+    if (newContact.id === '') {
+      Swal.fire({
+        icon: 'error',
+        title: 'Empty contact can not be added',
+      });
+      return;
+    }
+    contacts.push(newContact);
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+    $('tbody').html('');
+    renderTable();
     hideForm();
   });
   renderTable();
 });
 // delete item
-function deletItem() {
+function deleteContact(id) {
+  let contacts = JSON.parse(localStorage.getItem('contacts'));
+  contacts = contacts.filter(item => item.id != id);
+  localStorage.setItem('contacts', JSON.stringify(contacts));
+  $('tbody').html('');
+  renderTable();
+}
+function deletItem(id) {
   Swal.fire({
     title: 'Are you sure?',
     text: "You won't be able to revert this!",
@@ -64,26 +89,8 @@ function deletItem() {
     confirmButtonText: 'Yes, delete it!',
   }).then(result => {
     if (result.value) {
+      deleteContact(id);
       Swal.fire('Deleted!', 'Your file has been deleted.', 'success');
     }
-  });
-}
-// render table
-function renderTable() {
-  contacts.map((contact) => {
-    const tr = document.createElement('tr');
-    tr.innerHTML = `<td>${contact.id}</td>
-    <td>${contact.name}</td>
-    <td>${contact.surname}</td>
-  <td>${contact.phoneNumber}</td>
-  <td>${contact.email}</td>
-  <td>${contact.address}</td>
-  <td>${contact.birthday}</td>
-  <td>${contact.details}</td>
-  <td> <button type="button" onclick="deletItem()" class="btn btn-danger"><i
-  class="fa fa-trash" aria-hidden="true"></i></button>
-<button type="button" onclick="toggleForm()" class="btn btn-primary"><i
-  class="fa fa-pencil-square" aria-hidden="true"></i></button> </td>`;
-    return $('tbody').append(tr);
   });
 }
